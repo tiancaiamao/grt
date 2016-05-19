32089fc40488aede3d11427dfc5bd3df57fa3009
tiancaiamao <tiancaiamao@gmail.com>
2016-05-19 17:50:22 +0800

    恢复async的example

--- example/async_demo.lua
+++ example/async_demo.lua
@@ -1,10 +1,13 @@
+package.cpath = package.cpath .. ';../lib/?.so'
+package.path = package.path .. ';../lib/?.lua'
+
 local async = require "async"
 local EventLoop = async.EventLoop
 local Listener = async.Listener
 local Connection = async.Connection
 
 local server = Listener:new("8888")
-function server:on_conn(fd, event) 
+function server:on_conn(fd, event)
 	local c = Connection:new(fd)
 	function c:on_read(data)
 		self:write(data)
